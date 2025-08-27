c7067072ac51379d525979196cc69839f46f485a
Milos <dev980lop@gmail.com>
2025-08-27 17:15:55 +0200

    Refactoring JS logic of project page layout

--- action.js
+++ action.js
@@ -1,46 +1,9 @@
 "use strict";
 
-// Funkcia pre slideshow (nechávam tak ako máš)
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  if (slides.length) {
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-  }
-}
-
-// Funkcia na nastavenie projektu
-function setUp(name) {
-  localStorage.setItem('selectedProject', name);
-  window.location.href = './project.html';
-}
-
-
 
 let language = "language";
 let type = "type";
-let category= "category";
+let category = "category";
 
 const selectLanguageElement = document.getElementById('language-select');
 
@@ -74,7 +37,7 @@ if (selectCategoryElement) {
 
 
 function filter(value) {
-  
+
 
   const projects = document.querySelectorAll('div.project');
 
@@ -91,4 +54,145 @@ function filter(value) {
     }
   });
 }
+
+
+
+function setUp(projectName) {
+
+  localStorage.setItem('selectedProject', projectName);
+
+  window.location.href = './project.html';
+}
+
+
+
+
+let left = true;
+
+function projects(projectName) {
+const projects = new Map([
+  ["LIW", [
+    { img: "./assets/images/projects/liw/title.png", title: "Life Is Weird", desc: "Popis" },
+    { img: "./assets/images/projects/liw/1.png", title: "Nadpis", desc: "Popis" },
+    { img: "./assets/images/projects/liw/2.png", title: "Nadpis", desc: "Popis" },
+    { img: "./assets/images/projects/liw/3.png", title: "Nadpis", desc: "Popis" },
+    { img: "./assets/images/projects/liw/4.png", title: "Nadpis", desc: "Popis" }
+  ]],
+  ["EDU", [
+    { img: "./assets/images/projects/es/title.png", title: "Educational Software", desc: "Popis" },
+    { img: "./assets/images/projects/es/1.png", title: "Nadpis", desc: "Popis" },
+    { img: "./assets/images/projects/es/2.png", title: "Nadpis", desc: "Popis" },
+    { img: "./assets/images/projects/es/3.png", title: "Nadpis", desc: "Popis" },
+    { img: "./assets/images/projects/es/4.png", title: "Nadpis", desc: "Popis" }
+  ]],
+  ["HAN", [
+    { img: "./assets/images/projects/hangman/title.png", title: "Hangman", desc: "Popis" },
+    { img: "./assets/images/projects/hangman/1.png", title: "Nadpis", desc: "Popis" },
+    { img: "./assets/images/projects/hangman/2.png", title: "Nadpis", desc: "Popis" },
+    { img: "./assets/images/projects/hangman/3.png", title: "Nadpis", desc: "Popis" },
+    { img: "./assets/images/projects/hangman/4.png", title: "Nadpis", desc: "Popis" }
+  ]],
+  ["DIC", [
+    { img: "./assets/images/projects/dice/title.png", title: "Dice", desc: "Popis" },
+    { img: "./assets/images/projects/dice/1.png", title: "Nadpis", desc: "Popis" },
+    { img: "./assets/images/projects/dice/2.png", title: "Nadpis", desc: "Popis" },
+    { img: "./assets/images/projects/dice/3.png", title: "Nadpis", desc: "Popis" },
+    { img: "./assets/images/projects/dice/4.png", title: "Nadpis", desc: "Popis" }
+  ]]
+]);
+
+
+
+const project = projects.get(projectName);
+
+project.forEach(sub => {
+  createProject(sub.title, sub.desc, sub.img);
+});
+
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  projects(localStorage.getItem('selectedProject'));
+});
+
+function createProject(title, description, imageUrl) {
+
+
+  if (document.getElementsByClassName("project-description").length === 0) {
+
+    document.getElementById("title-image").src = imageUrl;
+    const project = document.createElement("div");
+    project.className = "project-description";
+
+    const itemHeader = document.createElement("div");
+    itemHeader.className = "item";
+
+    const h1 = document.createElement("h1");
+    h1.id = "title-name";
+    h1.textContent = title;
+
+    const p = document.createElement("p");
+    p.id = "title-desc";
+    p.textContent = description;
+
+    itemHeader.appendChild(h1);
+    itemHeader.appendChild(p);
+
+
+     const itemHeader2 = document.createElement("div");
+    itemHeader2.className = "item";
+
+    project.appendChild(itemHeader);
+project.appendChild(itemHeader2);
+    document.getElementById("project-description-section").appendChild(project);
+  
+    return;
+  }
+
+
+  // --- horná časť (title + desc) ---
+
+
+  // --- spodná časť (sekcia s obrázkom + textom) ---
+  const section = document.createElement("div");
+  section.className = "project-section";
+
+  // obrázok
+  const itemImg = document.createElement("div");
+  itemImg.className = "item";
+
+  const img = document.createElement("img");
+  img.className = "sub-image";
+  img.src = imageUrl;
+
+  itemImg.appendChild(img);
+
+  // text
+  const itemText = document.createElement("div");
+  itemText.className = "item";
+
+  const h5 = document.createElement("h5");
+  h5.className = "sub-title";
+  h5.textContent = title;
+
+  const pSub = document.createElement("p");
+  pSub.className = "sub-desc-a";
+  pSub.textContent = description;
+
+  itemText.appendChild(h5);
+  itemText.appendChild(pSub);
+
+  section.appendChild(left ? itemImg : itemText);
+  section.appendChild(left ? itemText : itemImg);
+
+  
+
+  document.getElementById("project-description-section").appendChild(section);
+
+  left = left ? false : true;
+}
+
+
+
+
 
--- project.js
+++ project.js
@@ -1,102 +1,41 @@
-const hangmanProject = new Map();
-hangmanProject.set("title-image", "./assets/images/projects/hangman/title.png");
-hangmanProject.set("title-name", "Hangman");
-hangmanProject.set("title-desc", "asdfdsafsadfsafdfasdfs");
-hangmanProject.set("sub-title-a", "asdfdsafsadfsafdfasdfs");
-hangmanProject.set("sub-desc-a", "asdfdsafsadfsafdfasdfs");
-hangmanProject.set("sub-image-a", "asdfdsafsadfsafdfasdfs");
 
-hangmanProject.set("sub-title-b", "asdfdsafsadfsafdfasdfs");
-hangmanProject.set("sub-desc-b", "asdfdsafsadfsafdfasdfs");
-hangmanProject.set("sub-image-b", "asdfdsafsadfsafdfasdfs");
+let slideIndex = 1;
+showSlides(slideIndex);
 
-hangmanProject.set("link", "asdfdsafsadfsafdfasdfs");
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-const dice = new Map();
-dice.set("title-image", "./assets/images/projects/dice/title.png");
-dice.set("title-name", "Dice");
-dice.set("title-desc", "asdfdsafsadfsafdfasdfs");
-dice.set("sub-title-a", "asdfdsafsadfsafdfasdfs");
-dice.set("sub-desc-a", "asdfdsafsadfsafdfasdfs");
-dice.set("sub-image-a", "asdfdsafsadfsafdfasdfs");
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-dice.set("sub-title-b", "asdfdsafsadfsafdfasdfs");
-dice.set("sub-desc-b", "asdfdsafsadfsafdfasdfs");
-dice.set("sub-image-b", "asdfdsafsadfsafdfasdfs");
-
-dice.set("link", "asdfdsafsadfsafdfasdfs");
-
-
-const liw = new Map();
-liw.set("title-image", "./assets/images/projects/liw/title.png");
-liw.set("title-name", "Life Is Weird");
-liw.set("title-desc", "asdfdsafsadfsafdfasdfs");
-liw.set("sub-title-a", "asdfdsafsadfsafdfasdfs");
-liw.set("sub-desc-a", "asdfdsafsadfsafdfasdfs");
-liw.set("sub-image-a", "asdfdsafsadfsafdfasdfs");
-
-liw.set("sub-title-b", "asdfdsafsadfsafdfasdfs");
-liw.set("sub-desc-b", "asdfdsafsadfsafdfasdfs");
-liw.set("sub-image-b", "asdfdsafsadfsafdfasdfs");
-
-liw.set("link", "asdfdsafsadfsafdfasdfs");
-
-
-const es = new Map();
-es.set("title-image", "./assets/images/projects/es/title.png");
-es.set("title-name", "Educational Software");
-es.set("title-desc", "asdfdsafsadfsafdfasdfs");
-es.set("sub-title-a", "asdfdsafsadfsafdfasdfs");
-es.set("sub-desc-a", "asdfdsafsadfsafdfasdfs");
-es.set("sub-image-a", "asdfdsafsadfsafdfasdfs");
-
-es.set("sub-title-b", "asdfdsafsadfsafdfasdfs");
-es.set("sub-desc-b", "asdfdsafsadfsafdfasdfs");
-es.set("sub-image-b", "asdfdsafsadfsafdfasdfs");
-
-es.set("link", "asdfdsafsadfsafdfasdfs");
-
-
-
-
-const projects = new Map();
-projects.set("hangman", hangmanProject);
-projects.set("dice", dice);
-projects.set("liw", liw);
-projects.set("es", es);
-
-
-
-
-window.addEventListener('DOMContentLoaded', () => {
-
-  const name = localStorage.getItem('selectedProject');
-  localStorage.removeItem('selectedProject');
-
-  if (!name) {
-    return;
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-
-
-  const project = projects.get(name);
-  if (!project) {
-    return;
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
   }
+  if (slides.length) {
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+  }
+}
 
-  project.forEach((value, key) => {
-    const element = document.getElementById(key);
-    if (!element) return;
 
-    if (element instanceof HTMLImageElement) {
 
-      element.src = value;
-    } else if (element instanceof HTMLParagraphElement || element instanceof HTMLHeadingElement) {
-      element.textContent = value;
-    } else if (element instanceof HTMLAnchorElement) {
-      element.href = value;
-    }
-  });
-});
+
+
+
+
+
+
 
 
 
