e75a8e4e4017ca303c454039a1c592f366a0639e
Milos <dev980lop@gmail.com>
2025-08-29 22:51:49 +0200

    Add SPA capability to the project.html

--- action.js
+++ action.js
@@ -1,6 +1,15 @@
 "use strict";
 
 
+const images = document.querySelectorAll('.project-section .item img');
+
+images.forEach(img => {
+    img.addEventListener('click', () => {
+        img.classList.toggle('enlarged');
+    });
+});
+
+
 let language = "language";
 let type = "type";
 let category = "category";
@@ -58,9 +67,7 @@ function filter(value) {
 
 
 function setUp(projectName) {
-
   localStorage.setItem('selectedProject', projectName);
-
   window.location.href = './project.html';
 }
 
@@ -72,18 +79,29 @@ let left = true;
 function projects(projectName) {
 const projects = new Map([
   ["LIW", [
-    { img: "./assets/images/projects/liw/title.png", title: "Life Is Weird", desc: "Popis" },
+    { img: "./assets/images/projects/liw/title.png", title: "Life Is Weird", desc: "Jednoduchá príbehová GUI aplikácia voľne inšpirovaná zážitkami z AAA hier Firewatch a Life Is Strange." },
     { img: "./assets/images/projects/liw/1.png", title: "Nadpis", desc: "Popis" },
     { img: "./assets/images/projects/liw/2.png", title: "Nadpis", desc: "Popis" },
     { img: "./assets/images/projects/liw/3.png", title: "Nadpis", desc: "Popis" },
-    { img: "./assets/images/projects/liw/4.png", title: "Nadpis", desc: "Popis" }
+    { img: "./assets/images/projects/liw/4.png", title: "Nadpis", desc: "Popis" },
+     { img: "./assets/images/projects/liw/5.png", title: "Nadpis", desc: "Popis" },
+      { img: "./assets/images/projects/liw/6.png", title: "Nadpis", desc: "Popis" }
   ]],
   ["EDU", [
-    { img: "./assets/images/projects/es/title.png", title: "Educational Software", desc: "Popis" },
-    { img: "./assets/images/projects/es/1.png", title: "Nadpis", desc: "Popis" },
-    { img: "./assets/images/projects/es/2.png", title: "Nadpis", desc: "Popis" },
-    { img: "./assets/images/projects/es/3.png", title: "Nadpis", desc: "Popis" },
-    { img: "./assets/images/projects/es/4.png", title: "Nadpis", desc: "Popis" }
+    { img: "./assets/images/projects/es/title.png", title: "Educational Software", desc: "Učebná pomôcka pre študentov predmetu Numerické metódy." },
+    { img: "./assets/images/projects/es/1.png", title: "Prihlasovací formulár", desc: "Možnosť prihlásenia majú len študenti predmenu Numerické metódy a zamestnanci fakulty."},
+    { img: "./assets/images/projects/es/2.png", title: "Registračný formulár", desc: "Možnosť registrácie majú len študenti daného predmetu a a zamestnanci fakulty. Implementácia validácii je riešena vlastnou logikou prostredníctvom vanilla JS. Bol použitý dynamicky sa meniaci validačný mechanizmus." },
+    { img: "./assets/images/projects/es/3.png", title: "Zabudnuté heslo", desc: "Popis"},
+    { img: "./assets/images/projects/es/4.png", title: "Hlavné menu", desc: "Zoznam numerických kategorii. Každá kategória obsahuje zoznam vybranych numerických metód na spracovanie."},
+    { img: "./assets/images/projects/es/5.png", title: "Materiály", desc: ""},
+    { img: "./assets/images/projects/es/6.png", title: "Komunikácia", desc: "Jednoduchý komunikačný kanál s vyučujúcim predmetu. Vyučujúci si môže vybrať so zoznamu študenta s ktorým bude prebiehať komunikácia. Študent má možnosť komunikovať len výlučne s vyučujúcim."},
+    { img: "./assets/images/projects/es/7.png", title: "Profil", desc: "Popis"},
+    { img: "./assets/images/projects/es/8.png", title: "System", desc: ""},
+    { img: "./assets/images/projects/es/9.png", title: "System", desc: "Zoznam funkcionalít, ktorým vyučujúci spravuje daný predmet. Cez toto registruje žiakov, nastavuje max počet absencii, bodov."},
+    { img: "./assets/images/projects/es/10.png", title: "Materiály", desc: "Zoznam materialov nahraných vyučujúcim predmetu. Študent si je schopný stiahnuť vybraný materiál. Vyučujúci je schopný nahrávať a odstraňovať súbory."},
+    { img: "./assets/images/projects/es/11.png", title: "Študenti", desc: "Zoznam zaregistrovaných študentov. Možnosť pridelovania dosiahnutých bodov a prípadných absencii."},
+    { img: "./assets/images/projects/es/12.png", title: "Zamestnanci", desc: "Zoznam zaregistrovaných zamestnancov. Slúži na prehľad a v prípade potreby aj dohľadania používateľského mena pre prípadnú zmenu vyučujúceho."},
+    { img: "./assets/images/projects/es/13.png", title: "Komunikácia", desc: "Jednoduchý komunikačný kanál s vyučujúcim predmetu. Vyučujúci si môže vybrať so zoznamu študenta s ktorým bude prebiehať komunikácia. Študent má možnosť komunikovať len výlučne s vyučujúcim."}
   ]],
   ["HAN", [
     { img: "./assets/images/projects/hangman/title.png", title: "Hangman", desc: "Popis" },
@@ -98,6 +116,16 @@ const projects = new Map([
     { img: "./assets/images/projects/dice/2.png", title: "Nadpis", desc: "Popis" },
     { img: "./assets/images/projects/dice/3.png", title: "Nadpis", desc: "Popis" },
     { img: "./assets/images/projects/dice/4.png", title: "Nadpis", desc: "Popis" }
+  ]],
+  ["SPR", [
+    { img: "./assets/images/projects/spr/title.png", title: "Swan Protocol", desc: "Popis" },
+    { img: "./assets/images/projects/spr/1.png", title: "Nadpis", desc: "Možnosť zvolenia si " },
+    { img: "./assets/images/projects/spr/2.png", title: "Nadpis", desc: "Popis" },
+    { img: "./assets/images/projects/spr/3.png", title: "Nadpis", desc: "Popis" },
+    { img: "./assets/images/projects/spr/4.png", title: "Nadpis", desc: "Popis" },
+    { img: "./assets/images/projects/spr/5.png", title: "Nadpis", desc: "Popis" },
+    { img: "./assets/images/projects/spr/6.png", title: "Nadpis", desc: "Popis" },
+    { img: "./assets/images/projects/spr/7.png", title: "Nadpis", desc: "Popis" }
   ]]
 ]);
 
@@ -169,7 +197,7 @@ project.appendChild(itemHeader2);
 
   // text
   const itemText = document.createElement("div");
-  itemText.className = "item";
+  itemText.className = "item item-text";
 
   const h5 = document.createElement("h5");
   h5.className = "sub-title";
